874badb4143d8871b83ecdd8f5e45fce4e290e02
Leo Hardy <leo_hardy@hotmail.co.uk>
2023-01-31 21:47:36 +0000

    added additional CSS

--- src/features/search/Search.js
+++ src/features/search/Search.js
@@ -1,36 +1,34 @@
-import { useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { updateKeyword } from "./searchSlice";
 
 export function Search({ submitKeyword }) {
     const dispatch = useDispatch();
     const stateKeyword = useSelector(state => state.search.keyword);
-    const [keyword, setKeyword] = useState("");
+
     const handleChange = (e) => {
         const value = e.target.value;
-        setKeyword(value);
+        dispatch(updateKeyword(value));
     }
     const handleSubmit = (e) => {
         e.preventDefault();
-        dispatch(updateKeyword(keyword));
-        submitKeyword(keyword);
+        submitKeyword(stateKeyword);
     }
 
     return (
-        <div className="container text-center">
+        <div className="headerContainer text-center">
             <header className="row justify-content-center">
                 <h1 className="col col-4">Reddit Searcher</h1>
-                <form className="col col-4 container px-4" onSubmit={handleSubmit}>
+                <form className="col col-2 search container" onSubmit={handleSubmit}>
                     <input
                         required
                         className="form-control"
                         type="search"
                         placeholder="search reddit"
                         onChange={handleChange}
-                        value={keyword}
+                        value={stateKeyword}
                     >
                     </input>
-                    <button className="col-sm-6 gx-5 w-100 btn btn-lg btn-outline-primary" type="submit">Search</button>
+                    <button className="btn btn-outline-primary" type="submit">Search</button>
                 </form>
             </header>
         </div>
