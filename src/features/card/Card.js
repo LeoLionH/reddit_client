8e66abafdf85fb152aa41c32bd9151274a8a0434
Leo Hardy <leo_hardy@hotmail.co.uk>
2023-02-05 16:53:37 +0000

    breakpoint tweak to css

--- src/features/card/Card.js
+++ src/features/card/Card.js
@@ -9,22 +9,21 @@ export function Card(props) {
         <div className="cardContainer" data-testid="card" >
             <a href={url} className="card mb-4 rounded-3 shadow-sm">
                 <h3 className="card-title pricing-card-title">{title}</h3>
-                <table className="row justify-content-center">
-                    <thead>
-                        <tr className="row justify-content-center">
-                            <th className="col-3" data-testid="Author">Author</th>
-                            <th className="col-3" data-testid="Upvotes">Upvotes</th>
-                            <th className="col-3" data-testid="Comments">Comments</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        <tr className="row justify-content-center">
-                            <td className="col-3" data-testid="AuthorResult">{author}</td>
-                            <td className="col-3" data-testid="UpvotesResult">{upvotes}</td>
-                            <td className="col-3" data-testid="CommentsResult">{comments}</td>
-                        </tr>
-                    </tbody>
-                </table>
+                <div className="cardListParent">
+                    <div className="cardList" id="Author">
+                        <h5 className="" data-testid="Author">Author</h5>
+                        <p className="" data-testid="AuthorResult">{author}</p>
+                    </div>
+                    <div className="cardList" id="Upvotes">
+                        <h5 className="" data-testid="Upvotes">Upvotes</h5>
+                        <p className="" data-testid="UpvotesResult">{upvotes}</p>
+                    </div>
+                    <div className="cardList" id="Comments">
+                        <h5 className="" data-testid="Comments">Comments</h5>
+                        <p className="" data-testid="CommentsResult">{comments}</p>
+                    </div>
+                </div>
+
             </a>
         </div>
     )
